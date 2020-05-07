d1735445fc3b8a82cde2867adc93558050706d9a
Natalya T <shevtsova.0113@gmail.com>
2020-05-08 03:09:15 +0700

    Login page tests added

---
+++ test_login_page.py
@@ -0,0 +1,19 @@
+from pages.login_page import LoginPage
+
+def test_guest_should_see_correct_url(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
+
+def test_guest_should_see_login_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+    page.open()                      # открываем страницу
+    page.should_be_login_form()          # выполняем метод страницы - переходим на страницу логина
+    
+def test_guest_should_see_register_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_register_form()
