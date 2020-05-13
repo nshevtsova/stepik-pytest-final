d5caf8f21aca52cd91765e48867edbe2d4fd953d
Natalya T <shevtsova.0113@gmail.com>
2020-05-14 02:27:57 +0700

    Negative tests added for product page, lesson 4.3 step 6

--- Pages/product_page.py
+++ Pages/product_page.py
@@ -10,6 +10,7 @@ class ProductPage(BasePage):
     def add_to_basket(self):
         button = self.find_element(*ProductPageLocators.ADD_TO_BASKET_BUTTON)
         button.click()
+        self.solve_quiz_and_get_code()
 
     def should_be_add_to_basket_success_message(self):
         assert self.is_element_present(*ProductPageLocators.PRODUCT_ADDED_TO_BASKET_SUCCESS_MESSAGE), "Success message is not presented"
@@ -24,3 +25,12 @@ class ProductPage(BasePage):
     def should_be_added_product_price_correct(self):
         assert self.get_element_text(*ProductPageLocators.BASKET_PRICE_IN_BASKET_PRICE_MESSAGE) \
                == self.get_element_text(*ProductPageLocators.PRODUCT_PRICE), "Product price in message is wrong"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*ProductPageLocators.PRODUCT_ADDED_TO_BASKET_SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def should_be_success_message_disappeared(self):
+        assert self.is_disappeared(*ProductPageLocators.PRODUCT_ADDED_TO_BASKET_SUCCESS_MESSAGE), \
+            "Success message is still presented, but should disappear"
+
--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,9 @@
 from pages.product_page import ProductPage
 import pytest
+import time
 
 
-@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+'''@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
@@ -13,20 +14,50 @@ import pytest
                                       "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
-def test_guest_can_add_product_to_basket(browser, link):
-    #link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+'''
+
+
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    # link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+
     page = ProductPage(browser, link)
     page.open()
     page.add_to_basket()
-
-    page.solve_quiz_and_get_code()
 
     page.should_be_add_to_basket_success_message()
     page.should_be_added_product_name_correct()
 
     page.should_be_basket_price_message()
     page.should_be_added_product_price_correct()
+
+
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+
+    page.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+
+    page.should_be_success_message_disappeared()
 
 
 
