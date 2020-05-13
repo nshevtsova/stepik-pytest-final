aa6f7d0a07313c046512fe5d2623b0e55e5242e5
Natalya T <shevtsova.0113@gmail.com>
2020-05-14 01:19:53 +0700

    Formatting corrected according to PyCharm

--- test_main_page.py
+++ test_main_page.py
@@ -9,13 +9,15 @@ def test_guest_can_go_to_login_page(browser):
     page.go_to_login_page()          # выполняем метод страницы - переходим на страницу логина
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
-    
+
+
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
-    
+
+
 def test_guest_should_see_correct_url(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
@@ -24,6 +26,7 @@ def test_guest_should_see_correct_url(browser):
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_url()
 
+
 def test_guest_should_see_login_form(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
@@ -31,7 +34,8 @@ def test_guest_should_see_login_form(browser):
     page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_form()          # выполняем метод страницы - переходим на страницу логина
-    
+
+
 def test_guest_should_see_register_form(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
