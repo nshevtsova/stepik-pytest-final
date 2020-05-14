33cf1a265c8980dd0c1a2681b839c6589278a94a
Natalya T <shevtsova.0113@gmail.com>
2020-05-14 15:50:59 +0700

    Basket page added

---
+++ Pages/basket_page.py
@@ -0,0 +1,16 @@
+from .base_page import BasePage
+from .locators import BasketPageLocators
+
+
+class BasketPage(BasePage):
+    def should_be_empty_basket_message(self):
+        assert self.is_element_present(*BasketPageLocators.EMPTY_BASKET_MESSAGE), "Empty basket message is not presented"
+
+    def should_not_be_empty_basket_message(self):
+        assert self.is_not_element_present(*BasketPageLocators.EMPTY_BASKET_MESSAGE), "Empty basket message is presented, but should not"
+
+    def should_be_products_list(self):
+        assert self.is_element_present(*BasketPageLocators.PRODUCTS_LIST_IN_BASKET), "Empty basket message is not presented"
+
+    def should_not_be_products_list(self):
+        assert self.is_not_element_present(*BasketPageLocators.PRODUCTS_LIST_IN_BASKET), "Empty basket message is presented, but should not"
--- Pages/locators.py
+++ Pages/locators.py
@@ -4,6 +4,12 @@ from selenium.webdriver.common.by import By
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BASKET_LINK = (By.CSS_SELECTOR, ".basket-mini>.btn-group>a.btn.btn-default")
+
+
+class BasketPageLocators():
+    EMPTY_BASKET_MESSAGE = (By.CSS_SELECTOR, "#content_inner>p")
+    PRODUCTS_LIST_IN_BASKET = (By.CSS_SELECTOR, "#basket_formset")
 
 
 class MainPageLocators():
